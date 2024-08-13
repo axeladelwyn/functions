6773963c3c41a5448a780efa5f9ab9022b6bc982
axeladelwyn <85653279+axeladelwyn@users.noreply.github.com>
2024-08-13 22:33:02 +0700

    added comments and statement-expression

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+// So we’re doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully, this comment will
+// explain what’s going on.
+
 fn main() {
     println!("Hello, world!");
 
     another_function(); //  You can call functions above the actual functions
 
     number_functions(20);
+
+    print_labeled_measurement(10, 'Z');
+
+    let y = {
+        let x = 5;
+        x + 2
+    };
+
+    println!("The value of y is: {y}");
+
+    let b = seven();
+
+    println!("The value of b is: {b}");
+
+    let x = plus_one(8);
+
+    println!("The value of x is {x}");
 }
 
 fn another_function() {
@@ -11,5 +32,17 @@ fn another_function() {
 }
 
 fn number_functions(x: i32) {
-    println!("THe value of x is {}", x);
+    println!("The value of x is {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value} {unit_label}");
+}
+
+fn seven() -> i32 {
+    7
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
